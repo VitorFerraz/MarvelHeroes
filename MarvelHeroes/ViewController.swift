8752e5c70f254516178d4819dae571fe1733d25f
Vitor Ferraz Varela <vitorfvarela@gmail.com>
2021-01-27 20:40:43 -0300

    Initial Commit

---
+++ MarvelHeroes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelHeroes
+//
+//  Created by Vitor Ferraz Varela on 27/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
